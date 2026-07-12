77f40e3124f2bfa764b916e8b42745270887ab5a
zenodea <zenolino@gmail.com>
2026-07-12 12:27:02 +0100

    chore: regenerated configs for dracula

--- mac/config/sketchybar/colors.sh
+++ mac/config/sketchybar/colors.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-export BLACK=0xff1e1e2e
-export WHITE=0xffbac2de
-export RED=0xfff38ba8
-export GREEN=0xffa6e3a1
-export BLUE=0xff89dceb
-export YELLOW=0xfff9e2af
-export ORANGE=0xfffab387
-export MAGENTA=0xffcba6f7
-export GREY=0xff585b70
+export BLACK=0xff282a36
+export WHITE=0xffffffff
+export RED=0xffff5555
+export GREEN=0xff50fa7b
+export BLUE=0xffbd93f9
+export YELLOW=0xfff1fa8c
+export ORANGE=0xffffb86c
+export MAGENTA=0xffff79c6
+export GREY=0xff6272a4
 export TRANSPARENT=0x00000000
 
 # General bar colors
 export BAR_COLOR=$TRANSPARENT
 export ICON_COLOR=$WHITE
 export LABEL_COLOR=$WHITE
-export BACKGROUND_1=0x9045475a
-export BACKGROUND_2=0x90585b70
-export POPUP_BACKGROUND_COLOR=0xff1e1e2e
+export BACKGROUND_1=0x9044475a
+export BACKGROUND_2=0x906272a4
+export POPUP_BACKGROUND_COLOR=0xff282a36
 export POPUP_BORDER_COLOR=$WHITE
 export SHADOW_COLOR=$BLACK
