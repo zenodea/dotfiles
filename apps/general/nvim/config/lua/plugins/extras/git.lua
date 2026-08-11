ef9f90c58de606a7360cabb861089e3f983a21dd
zenodea <zenolino@gmail.com>
2026-08-11 19:33:58 +0100

    fix: diffview vs main opens editable buffers with --imply-local

--- apps/general/nvim/config/lua/plugins/extras/git.lua
+++ apps/general/nvim/config/lua/plugins/extras/git.lua
@@ -6,7 +6,9 @@ return {
     cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' },
     keys = {
       { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff view' },
-      { '<leader>gm', '<cmd>DiffviewOpen origin/main...HEAD<cr>', desc = '[G]it diff vs [M]ain' },
+      -- --imply-local: the HEAD side shows the working-tree files, so they
+      -- are editable and have LSP, instead of read-only revision buffers
+      { '<leader>gm', '<cmd>DiffviewOpen origin/main...HEAD --imply-local<cr>', desc = '[G]it diff vs [M]ain' },
       { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it file [H]istory' },
       { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it diff [Q]uit' },
     },
